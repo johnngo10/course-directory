b524b109b5242a8a2920bd875f41ce3b748fc1a3
xuanngo10 <xuan.ngo10@gmail.com>
2020-05-02 18:50:44 -0700

    first init

---
+++ build/precache-manifest.144aac8132311f5b02285ad467da366d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e96130426e0805366e31722825a413b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7d4bb3f8d25f13417aa",
+    "url": "/static/css/main.3a22b66b.chunk.css"
+  },
+  {
+    "revision": "fbcfca28e07f3e3c7797",
+    "url": "/static/js/2.8655f4ec.chunk.js"
+  },
+  {
+    "revision": "246ad901f08703825af5eeab9ed7a0eb",
+    "url": "/static/js/2.8655f4ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7d4bb3f8d25f13417aa",
+    "url": "/static/js/main.7a6f9d29.chunk.js"
+  },
+  {
+    "revision": "ffab2b94da18f8218f4b",
+    "url": "/static/js/runtime-main.3c081a17.js"
+  }
+]);
